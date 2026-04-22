e01c6dc65db1ebfe842832d88d5b080b9446d7a9
Prak005 <prakharmallick005@gmail.com>
2026-04-22 15:12:00 +0530

    fix user name

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 module.exports = new Pool ({
     host: process.env.DB_HOST,
-    user: process.env.DB_NAME,
+    user: process.env.DB_USER,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
