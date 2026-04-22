5a99f62f436240f9624c6e4536c968836ade4a51
Prak005 <prakharmallick005@gmail.com>
2026-04-22 22:48:31 +0530

    throw error when same email exists

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,7 +14,13 @@ router.post('/sign-up',
     [
         body('firstName').trim().notEmpty().withMessage('First Name required'),
         body('lastName').trim().notEmpty().withMessage('Last Name required'),
-        body('email').isEmail().withMessage('Invalid Email'),
+        body('email').isEmail().withMessage('Invalid Email').custom(async (email) => {
+            const user = await db.getUserByEmail(email);
+            if (user) {
+                throw new Error('User already exists');
+            }
+            return true;
+        }),
         body('password').isLength({min:6}).withMessage('Password must be atleast 6 characters long'),
         body('confirmPassword').custom((value, {req}) => {
             if(value !== req.body.password){
