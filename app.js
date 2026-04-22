262a31a473cabab20b9de26a456fbdb01a6785c6
Prak005 <prakharmallick005@gmail.com>
2026-04-22 23:24:12 +0530

    add passport and session table

--- app.js
+++ app.js
@@ -4,13 +4,26 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+const passport = require('./config/passport');
+const pool = require('./db/pool')
 const authRoutes = require('./routes/authRoutes');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 
-app.use(session({ secret: 'cats', resave: false, saveUninitialized: false}));
+app.use(session({
+    store: new pgSession({
+        pool: pool,
+        tableName: 'session',
+    }), 
+    secret: 'cats', 
+    resave: false, 
+    saveUninitialized: false
+}));
 app.use(express.urlencoded({ extended:false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', authRoutes);
 app.get('/', (req, res) => {
