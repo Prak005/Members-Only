c60551284da7755d5bf6ee0f730fea57a7227c3b
Prak005 <prakharmallick005@gmail.com>
2026-04-22 13:22:06 +0530

    add query to create table

---
+++ scripts/seed.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const { Client } = require('pg');
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    is_member BOOLEAN DEFAULT FALSE,
+    is_admin BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+`;
+
+async function main(){
+    console.log('seeding...');
+    const client = new Client({
+        connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log('done');
+}
+main();
