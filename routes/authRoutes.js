023a056d31e49c20f6b1d4ebca89535ae8cba821
Prak005 <prakharmallick005@gmail.com>
2026-04-22 15:12:44 +0530

    add post sign-up

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,34 @@
 const { Router } = require('express');
 const router = Router();
+const bcrypt = require('bcrypt');
+const { body, validationResult } = require('express-validator');
+const db = require('../db/queries');
 
-router.get('sign-up', (req, res) => {
+router.get('/sign-up', (req, res) => {
     res.render('sign-up');
 });
-
+router.post('/sign-up',
+    [
+        body('firstName').trim().notEmpty().withMessage('First Name required'),
+        body('lastName').trim().notEmpty().withMessage('Last Name required'),
+        body('email').isEmail().withMessage('Invalid Email'),
+        body('password').isLength({min:6}).withMessage('Password must be atleast 6 characters long'),
+        body('confirmPassword').custom((value, {req}) => {
+            if(value !== req.body.password){
+                throw new Error('Passwords do not match');
+            }
+            return true;
+        }),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.send(errors.array());
+        }
+        const { firstName, lastName, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await db.createUser({firstName, lastName, email, password:hashedPassword});
+        res.send('New User Created');
+    }
+);
 module.exports = router;
