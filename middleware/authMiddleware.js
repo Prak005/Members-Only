0c3c0e42be08f79a9a4ca28c7075622332a74d3a
Prak005 <prakharmallick005@gmail.com>
2026-04-23 10:11:03 +0530

    add authentication

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,10 @@
+function isAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/log-in');
+}
+
+module.exports = {
+    isAuthenticated,
+};
